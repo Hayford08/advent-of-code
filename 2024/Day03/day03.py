1919375e036abdfc9e387a4cad563e4fcda9ace1
Hayford Akomeah <hayford@mit.edu>
2024-12-03 00:15:48 -0500

    Day 3

--- 2024/Day03/day03.py
+++ 2024/Day03/day03.py
@@ -1,4 +1,5 @@
 from collections import Counter, deque, defaultdict
+import re
 import os
 import sys
 import math
@@ -17,16 +18,31 @@ def get_input():
 
 def part1():
     ans = 0
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
     for line in get_input():
-        pass 
+        matches = re.findall(pattern, line)
+        for match in matches:
+            ans += int(match[0]) * int(match[1])
     return ans
 
 def part2():
     ans = 0
+    pattern = r"(?P<mul>mul\((\d{1,3}),\s*(\d{1,3})\))|(?P<do>do\(\))|(?P<dont>don't\(\))"
+    flag = True
     for line in get_input():
-        pass
+        matches = re.finditer(pattern, line)
+        for match in matches:
+            if match.group("mul") and flag:
+                values = match.group("mul")[:-1]
+                values = values[4:].split(",")
+                ans += int(values[0]) * int(values[1])
+            elif match.group("do"):
+                flag = True
+            elif match.group("dont"):
+                flag = False
+            
     return ans
 
 if __name__ == "__main__":
-    print(part1())
+    # print(part1())
     print(part2())
