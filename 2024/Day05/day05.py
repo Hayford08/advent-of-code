8d2ee095e95a11811a8b9bd52ef3b3ab564ae33a
Hayford Akomeah <hayford@mit.edu>
2024-12-05 01:06:25 -0500

    Day 5

--- 2024/Day05/day05.py
+++ 2024/Day05/day05.py
@@ -1,4 +1,5 @@
 from collections import Counter, deque, defaultdict
+from functools import cmp_to_key
 import os
 import sys
 import math
@@ -13,20 +14,55 @@ except:
 
 def get_input():
     with open("input.txt", "r") as fp:
-        return fp.readlines()
+        return fp.read().strip().split("\n\n")
+    
+def get_graph(section):
+    graph = defaultdict(set)
+    for line in section.split('\n'):
+        u, v = line.split('|')
+        graph[u].add(v)
+    return graph
+
+def good(graph, nodes):
+    n = len(nodes)
+    for i in range(n):
+        u = nodes[i]
+        for j in range(i + 1, n):
+            v = nodes[j]
+            if u in graph[v]:
+                return False
+    return True
 
 def part1():
     ans = 0
-    for line in get_input():
-        pass 
+    section1, section2 = get_input()
+    graph = get_graph(section1)
+    for line in section2.split('\n'):
+        nodes = line.strip().split(',')
+        if good(graph, nodes):
+            ans += int(nodes[len(nodes) // 2])
     return ans
 
 def part2():
     ans = 0
-    for line in get_input():
-        pass
+    section1, section2 = get_input()
+    graph = get_graph(section1)
+
+    def cmp(u, v):
+        if u in graph[v]:
+            return -1
+        if v in graph[u]:
+            return 1
+        return 0
+
+    for line in section2.split('\n'):
+        nodes = line.strip().split(',')
+        if good(graph, nodes):
+            continue
+        order = sorted(nodes, key=cmp_to_key(cmp))
+        ans += int(order[len(nodes) // 2])
     return ans
 
 if __name__ == "__main__":
-    print(part1())
+    # print(part1())
     print(part2())
